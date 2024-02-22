27574c60d29ed5b4c9029e955e861451bc76d003
Tyler Meekel <ty.meekel@gmail.com>
2024-02-22 13:21:30 -0700

    feat: add Application struct and run method to Gazelle application
    The Application struct is added to serve as the entry point for the Gazelle application. It contains a run method that will be used to start the application.

---
+++ src/application.rs
@@ -0,0 +1,10 @@
+// Application is the entry point for a Gazelle application
+pub struct Application {
+
+}
+
+impl Application {
+    pub fn run(&mut self) {
+        println!("This is running!")
+    }
+}
