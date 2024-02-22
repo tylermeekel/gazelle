e069840c09b33de3186071808466a2a828dc5d08
Tyler Meekel <ty.meekel@gmail.com>
2024-02-22 13:29:39 -0700

    feat: refactor application entry point to use Application struct
    The application entry point has been refactored to use the Application struct. This struct serves as the entry point for a Gazelle application. The `build` method has been added to the Application struct to handle the initialization of the struct and return a Result indicating success or failure. The `run` method has been moved to the Application struct and is now called on an instance of the struct.
    
    The contents of the application module have been moved back into lib.rs (for now)

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,19 @@
-// Declare Modules
-pub mod application;
+// Application is the entry point for a Gazelle application
+pub struct Application {
+
+}
+
+#[derive(Debug)]
+pub enum ApplicationBuildError {
+
+}
+
+impl Application {
+    pub fn build() -> Result<Self, ApplicationBuildError> {
+        Ok(Application{})
+    }
+
+    pub fn run(&mut self) {
+        println!("This is running!")
+    }
+}
