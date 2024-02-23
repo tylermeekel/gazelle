3d62ccdca72d677021696ae52dfac4d7187fb6f8
Tyler Meekel <ty.meekel@gmail.com>
2024-02-23 15:07:18 -0700

    Add event types and categories

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -1,4 +1,48 @@
 // Submodules
 mod application;
 mod window;
-mod input;
+mod mouse;
+mod keyboard;
+
+// Helper function for creating bit flags
+const fn BIT(n: u32) -> isize {
+    1 << n
+}
+
+// Describes different supported event types
+pub enum EventType {
+    // Window Event Types
+    WindowClose,
+    WindowResize,
+    WindowFocus,
+    WindowLostFocus,
+    WindowMoved,
+    // App Event Types
+    AppTick,
+    AppUpdate,
+    AppRender,
+    // Key Event Types
+    KeyPressed,
+    KeyReleased,
+    // Mouse Event Types
+    MouseButtonPressed,
+    MouseButtonReleased,
+    MouseMoved,
+    MouseScrolled,
+
+    // TODO: Implement Gamepad Support
+}
+
+pub enum EventCategoryFlag {
+    EventCategoryApplication = 0,
+    EventCategoryInput = BIT(0),
+    EventCategoryKeyboard = BIT(1),
+    EventCategoryMouse = BIT(2),
+    EventCategoryMouseButton = BIT(3),
+
+    // TODO: Implement Gamepad Category
+}
+
+pub trait Event {
+    fn get_event_category_flags(&self) -> isize;
+}
